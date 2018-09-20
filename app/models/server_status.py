535cceb35d169e3a0f46c4b6258b2a834e6749d9
spl3en <spl3en.contact@gmail.com>
2018-09-20 02:29:32 +0200

    Add catch for unreachable endpoints

--- app/models/server_status.py
+++ app/models/server_status.py
@@ -3,7 +3,16 @@ import certifi
 import json
 
 def get_status (server):
-    path = certifi.where()
-    url = server + "/api/v1/status/peer/"
-    r = requests.get(url, verify=path)
-    return json.loads(r.text)
+    try:
+        path = certifi.where()
+        url = server + "/api/v1/status/peer/"
+        r = requests.get(url, verify=path)
+        return json.loads(r.text)
+    except:
+        return {
+            'status' : 'offline : Endpoint unreachable',
+            'consensus' : 'Unknown',
+            'block_height' : 'Unknown',
+            'total_tx' : 'Unknown',
+            'unconfirmed_tx' : 'Unknown',
+        }
