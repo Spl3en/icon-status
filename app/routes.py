ccd78a457474345cd89c7d47830f3d157c480724
spl3en <spl3en.contact@gmail.com>
2018-10-15 21:45:43 +0200

    Add new testnets

--- app/routes.py
+++ app/routes.py
@@ -12,7 +12,9 @@ def index():
     servers = []
     servers.append (view.status (model.status ("https://ctz.solidwallet.io"), "Mainnet 1"))
     servers.append (view.status (model.status ("https://wallet.icon.foundation"), "Mainnet 2"))
-    servers.append (view.status (model.status ("https://bicon.net.solidwallet.io"), "Testnet for DApps"))
+    servers.append (view.status (model.status ("https://bicon.net.solidwallet.io"), "Testnet for DApps 1"))
+    servers.append (view.status (model.status ("https://cicon.net.solidwallet.io"), "Testnet for DApps 2"))
+    servers.append (view.status (model.status ("https://dicon.net.solidwallet.io"), "Testnet for DApps 3"))
     servers.append (view.status (model.status ("https://test-ctz.solidwallet.io"), "Testnet for Exchanges"))
     servers.append (view.status (model.status ("http://13.209.103.183:9000"), "ICON devteam net"))
 
